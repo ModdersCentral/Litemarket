e5793e12fb02e779df722664cd0cfba0c2d5f2e0
ModdersCentral <webmaster@modderscentral.com>
2016-11-10 19:39:45 +0000

    simple install file

--- uploadv1.0.0.0/public_html/install/index.php
+++ uploadv1.0.0.0/public_html/install/index.php
@@ -1,4 +1,42 @@
-<form>
+<?php
+if(file_exists('./LOCK')){die('delete the \'LOCK\' file so the installer can start. if you already installed the software then just ignore this');}  
+
+
+if (isset($_POST['submit'])){
+	$myfile = fopen("config1.php", "w") or die("Unable to open file!");
+	$txt = "
+	
+	
+		\$mysqlhost=\"$_POST['mysql_host']\";
+		\$mysqluser=\"$_POST['mysql_username']\";
+		\$mysqlpass=\"$_POST['mysql_password']\";
+		\$mysqldb=\"$_POST['mysql_db']\";
+	
+		\$rpcUser =\"$_POST['rpc_username']\";
+		\$rpcPass =\"$_POST['rpc_password']\";
+		\$rpcurl =\"$_POST['rpc_url']\";
+	
+		\$allowreg=$_POST['vendor_reg'];
+	
+		\$emalvalidpass='supersecurepassword';
+		\$sitelongnme=\"$_POST['general_name']\";
+		\$emailverifSTATUS=true;
+	
+		\$debug='none';
+	
+	"; 
+	fwrite($myfile, $txt);
+	fclose($myfile);
+	$myfile2 = fopen("LOCK", "w") or die("Unable to open file!");
+	fclose($myfile2);
+}
+
+
+
+
+?>
+
+<form method="post">
 <h1>mysql</h1>
   mysql host:<br>
   <input type="text" name="mysql_host"><br>
@@ -24,11 +62,9 @@
   
   
    allow vendor registrations:<br>
-   Yes<input type="radio" name="vendor_reg" value="male">
-  No<input type="radio" name="vendor_reg" value="female"><br>
+   Yes<input type="radio" name="vendor_reg" value="1">
+  No<input type="radio" name="vendor_reg" value="0"><br>
    
    
-  
-  
-  
- </form>
+  <input type="submit" name="submit" />
+</form>
