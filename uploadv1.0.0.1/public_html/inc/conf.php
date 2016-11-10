9e4ad59cc108dbcff54b86e52d77ea10a913235a
ModdersCentral <webmaster@modderscentral.com>
2016-11-10 04:49:17 +0000

    more files

---
+++ uploadv1.0.0.1/public_html/inc/conf.php
@@ -0,0 +1,15 @@
+<?php
+
+//whether to get info from daemon or website
+$settings["setting_api_type"] = 0;//(0 - daemon,1 - website)
+//the time which the price updates in millisceonds, 
+$settings["setting_update_time"] = 21600000;//default: 6 hours
+//whether the captcha is enabled
+$settings["setting_captcha_enabled"] = 0;
+//hide error details and display a user friendly error
+$settings["setting_friendly_errors_enabled"] = 0;
+//search engine friendly urls
+$settings["setting_seo_friendly_pages"] = 0;
+
+
+?>
