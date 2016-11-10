827da08eec49b0b8f9596f1103e755d70721cca9
ModdersCentral <webmaster@modderscentral.com>
2016-11-10 18:20:10 +0000

    install script

---
+++ uploadv1.0.0.0/public_html/install/index.php
@@ -0,0 +1,34 @@
+<form>
+<h1>mysql</h1>
+  mysql host:<br>
+  <input type="text" name="mysql_host"><br>
+   mysql username:<br>
+  <input type="text" name="mysql_username"><br>
+  mysql password:<br>
+  <input type="text" name="mysql_password"><br>
+  mysql db:<br>
+  <input type="text" name="mysql_db"><br>
+  
+  
+  <h1>bitcoin rpc</h1>
+   rpc user:<br>
+  <input type="text" name="rpc_username"><br>
+     rpc password:<br>
+  <input type="text" name="rpc_password"><br>
+     rpc url:<br>
+  <input type="text" name="rpc_url"><br>
+  
+  <h1>general</h1>
+    website name:<br>
+  <input type="text" name="general_name"><br>
+  
+  
+   allow vendor registrations:<br>
+   Yes<input type="radio" name="vendor_reg" value="male">
+  No<input type="radio" name="vendor_reg" value="female"><br>
+   
+   
+  
+  
+  
+ </form>
